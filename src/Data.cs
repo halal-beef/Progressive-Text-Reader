70d49da373991b9d847bcfc513ce8ad508d4d3d0
usrDottik <81375555+usrDottik@users.noreply.github.com>
2021-12-03 18:40:56 -0300

    Apply Latest Update!

--- src/Data.cs
+++ src/Data.cs
@@ -8,7 +8,7 @@ namespace Dottik.PTR
         public static string latestVersion = "";
         public static string downloadLink = "https://github.com/usrDottik/Progressive-Text-Reader/releases/download/Release/Progressive-Text-Reader.zip";
         public static string programMode = "defaultExecution";
-        public static readonly int versionCode = 1;
+        public static readonly int versionCode = 3;
         public static readonly string applicationVersion = "1.0";
         public static readonly string executionPath = Environment.CurrentDirectory + @"\";
         public static readonly string fileName = "read-this.txt";
